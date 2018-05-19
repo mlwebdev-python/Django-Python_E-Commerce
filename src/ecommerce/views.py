68bc74b457c720a995143245dac8a73a82afece5
mlwebdev-react <mlwebdev-react@outlook.com>
2018-05-19 09:30:56 -0400

    Initial commit

---
+++ src/ecommerce/views.py
@@ -0,0 +1,58 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home_page(request):
+  context = {
+    "title": "Hello World !",
+    "content": "Welcome to the Home page"
+  }
+  return render(request, "home_page.html", context)
+  
+def about_page(request):
+  context = {
+    "title": "About Page",
+    "content": "Welcome to About the page"
+  }
+  return render(request, "home_page.html", context)
+  
+def contact_page(request):
+  context = {
+    "title": "Contact Page",
+    "content": "Welcome to Contact the page"
+  }
+  if request.method == "POST":
+    # print(request.POST)
+    print(request.POST.get("fullname"))
+    print(request.POST.get("email"))
+    print(request.POST.get("content"))
+  return render(request, "contact/view.html", context)
+
+
+
+def home_page_old(request):
+  html_ = """
+  <!doctype html>
+  <html lang="en">
+    <head>
+      <!-- Required meta tags -->
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+      <!-- Bootstrap CSS -->
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+
+      <title>Hello, world!</title>
+    </head>
+    <body>
+      <div class="text-center">
+        <h1>Hello, world!</h1>
+      </div>
+      <!-- Optional JavaScript -->
+      <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+    </body>
+  </html>
+  """
+  return HttpResponse(html_)
