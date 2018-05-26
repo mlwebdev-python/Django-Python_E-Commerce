13beaaa14c2eafcd3bb6f1b4d0b2d1d4d096993c
mlwebdev-react <mlwebdev-react@outlook.com>
2018-05-26 13:57:46 -0400

    Create register page / route / validations / authentication
    Create register page / route / validations / authentication
    Validations in forms, routes in urls page, authentication on home page.

--- src/ecommerce/views.py
+++ src/ecommerce/views.py
@@ -1,23 +1,30 @@
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, get_user_model
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
+from .forms import ContactForm, LoginForm, RegisterForm 
 
-from .forms import ContactForm, LoginForm
+
 
 def home_page(request):
   context = {
     "title": "Hello World !",
     "content": "Welcome to the Home page"
   }
+  if request.user.is_authenticated():
+    context["premium_content"] = "YESSSS!"
   return render(request, "home_page.html", context)
   
+
+
 def about_page(request):
   context = {
     "title": "About Page",
     "content": "Welcome to About the page"
   }
   return render(request, "home_page.html", context)
-  
+
+
+
 def contact_page(request):
   contact_form = ContactForm(request.POST or None)
   context = {
@@ -27,12 +34,13 @@ def contact_page(request):
   }
   if contact_form.is_valid():
     print(contact_form.cleaned_data)
-  # if request.method == "POST":
-    # print(request.POST)
-    # print(request.POST.get("fullname"))
-    # print(request.POST.get("email"))
-    # print(request.POST.get("content"))
+  ''' # if request.method == "POST":
+      # print(request.POST)
+      # print(request.POST.get("fullname"))
+      # print(request.POST.get("email"))
+      # print(request.POST.get("content"))'''
   return render(request, "contact/view.html", context)
+
 
 
 def login_page(request):
@@ -54,17 +62,28 @@ def login_page(request):
       login(request, user)
       # Redirect to a success page.
       # context["form"] = LoginForm()
-      return redirect("/login")
+      return redirect("/")
     else:
       print("error")
   return render(request, "auth/login.html", context)
 
 
+User = get_user_model()
 def register_page(request):
   form = RegisterForm(request.POST or None)
-  if form.is_valid():
+  context = {
+    "form": form
+  }
+  if form.is_valid(): 
     print(form.cleaned_data)
-  return render(request, "auth/register.html", {})
+    username = form.cleaned_data.get("username")
+    email = form.cleaned_data.get("email")
+    password = form.cleaned_data.get("password")
+    new_user = User.objects.create_user(username, email, password)
+    print(new_user)
+  return render(request, "auth/register.html", context)
+
+
 
 def home_page_old(request):
   html_ = """
